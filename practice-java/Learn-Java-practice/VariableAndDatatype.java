c8e56548ea8d65d2dc0b448f5026151f37b79a68
Araf Desktop main <main.desktop@araf.com>
2026-09-26 13:04:47 +0600

    home

---
+++ practice-java/Learn-Java-practice/VariableAndDatatype.java
@@ -0,0 +1,10 @@
+public class VariableAndDatatype{
+    public static void main(String[] args) {
+        int age;
+
+        age = 12;
+        age = 30;
+
+        System.out.println(age);
+    }
+}
